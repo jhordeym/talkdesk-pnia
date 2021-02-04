6a2d95dad542f35cd0bc8db70f151c686810fb82
Jhordeym V. Santos <jhordeym.santos@devoteam.com>
2021-02-04 11:29:51 +0000

    pnia v1.1 - improved performance with caching of prefixes on startup

--- src/main/java/com/talkdesk/pnia/service/PrefixReader.java
+++ src/main/java/com/talkdesk/pnia/service/PrefixReader.java
@@ -6,12 +6,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Objects;
+import java.util.Set;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -20,12 +24,24 @@ public class PrefixReader {
     @Value("${app.prefixes.fileName}")
     private String filePath;
 
-    public void readPrefixesAndApply(final Consumer<String> consumer) throws AggregatorServiceException {
+    private Set<String> cachedPrefixes;
+
+    @PostConstruct
+    public void readPrefixesAndCache() throws AggregatorServiceException {
         try (InputStream is = getClass().getResourceAsStream(filePath)) {
             final BufferedReader br = new BufferedReader(new InputStreamReader(Objects.requireNonNull(is)));
-            br.lines().parallel().filter(PhoneNumberUtils::isValidPrefix).forEach(consumer::accept);
+            cachedPrefixes = br.lines().filter(PhoneNumberUtils::isValidPrefix).collect(Collectors.toSet());
         } catch (final IOException e) {
             throw new AggregatorServiceException("Couldn't read file: " + filePath, e);
         }
+    }
+
+    @PreDestroy
+    public void clearCache() {
+        if (Objects.nonNull(cachedPrefixes)) cachedPrefixes = null;
+    }
+
+    public void readPrefixesAndApply(final Consumer<String> consumer) {
+        if (Objects.nonNull(cachedPrefixes)) cachedPrefixes.parallelStream().forEach(consumer);
     }
 }
